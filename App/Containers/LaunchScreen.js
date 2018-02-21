2b6ecc4ba5938013c7f2ddf115140753c5594674
hannanshaik <hannan.shaik@gmail.com>
2018-02-21 23:04:28 +0530

    Added iOS support for login to twitter.

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, Image, View } from 'react-native'
 import { Images } from '../Themes'
-
+import OAuthManager from 'react-native-oauth';
 // Styles
 import styles from './Styles/LaunchScreenStyles'
 
+const config = {
+  twitter: {
+    consumer_key: 'oNKtP1e5SkuJ8Gi4CFLtyQP2x',
+    consumer_secret: 'ZBl1K0e3jQksqaokb4zy6phNK5lNBcwgvyT948brD0IA3YAwbj'
+  }
+}
+// Create the manager
+const manager = new OAuthManager('hannantwitterapp')
+// configure the manager
+manager.configure(config);
+
 export default class LaunchScreen extends Component {
-  render () {
+
+  componentWillMount() {
+    console.log('hello');
+    manager.authorize('twitter')
+      .then(resp => {
+        console.log(resp)
+        const userTimelineUrl = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+        manager
+          .makeRequest('twitter', userTimelineUrl)
+          .then(resp => {
+            console.log('Data ->', resp.data);
+            manager.deauthorize('twitter');
+
+          });
+      }
+      )
+      .catch(err => console.log(err));
+  }
+
+  render() {
     return (
       <View style={styles.mainContainer}>
         <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
--- ios/TwitterApp/AppDelegate.m
+++ ios/TwitterApp/AppDelegate.m
@@ -8,6 +8,7 @@
  */
 
 #import "AppDelegate.h"
+#import "OAuthManager.h"
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -27,11 +28,24 @@
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
+
   self.window.rootViewController = rootViewController;
+  
+  [OAuthManager setupOAuthHandler:application];
+
   [self.window makeKeyAndVisible];
+
   return YES;
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+  return [OAuthManager handleOpenUrl:application
+                             openURL:url
+                   sourceApplication:sourceApplication
+                          annotation:annotation];
 }
 
 @end
